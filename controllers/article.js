b1a8e8045520d32b95ad524b7783e80af54efdcb
David <davp123.dh@gmail.com>
2021-06-23 15:17:36 -0400

    manejo d imagenes añadido

--- controllers/article.js
+++ controllers/article.js
@@ -2,7 +2,10 @@
 
 
 var validator = require('validator');
-const { validate } = require('../models/artilcle');
+var fs = require('fs');
+var path = require('path');
+
+const { validate, exists } = require('../models/artilcle');
 var Article = require('../models/artilcle');
 var controller = {
 
@@ -140,23 +143,23 @@ var controller = {
         //validar los datos
         try {
             var validate_title = !validator.isEmpty(params.title);
-            var validate_content=!validator.isEmpty(params.content);
+            var validate_content = !validator.isEmpty(params.content);
         } catch (err) {
             return res.status(200).send({
                 status: 'error',
                 message: 'Faltan datos por enviar'
             });
         }
-        if(validate_title && validate_content){
+        if (validate_title && validate_content) {
             //hacer a consulta find & update
-            Article.findOneAndUpdate({_id: articleId},params,{new:true},(err,articleUpdate)=>{
-                if(err){
+            Article.findOneAndUpdate({ _id: articleId }, params, { new: true }, (err, articleUpdate) => {
+                if (err) {
                     return res.status(500).send({
                         status: 'error',
                         message: 'Error al actualizar'
                     });
                 }
-                if(!articleUpdate){
+                if (!articleUpdate) {
                     return res.status(404).send({
                         status: 'error',
                         message: 'No existe el articulo'
@@ -167,31 +170,31 @@ var controller = {
                     article: articleUpdate
                 });
             })
-        }else{
+        } else {
             return res.status(200).send({
                 status: 'error',
                 message: 'Faltan datos por enviar'
             });
         }
-       
-       
+
+
     },
 
-    delete: (req, res)=>{
+    delete: (req, res) => {
 
         //recoger el id de la url
-        var articleId= req.params.id;
+        var articleId = req.params.id;
 
         // find and delete
-        Article.findOneAndDelete({_id: articleId},(err,articleRemoved)=>{
+        Article.findOneAndDelete({ _id: articleId }, (err, articleRemoved) => {
 
-            if(err){
+            if (err) {
                 return res.status(500).send({
                     status: 'error',
                     message: 'Error al eliminar'
                 });
             }
-            if(!articleRemoved){
+            if (!articleRemoved) {
                 return res.status(404).send({
                     status: 'error',
                     message: 'No se ha borrado el articulo, no existe'
@@ -202,8 +205,78 @@ var controller = {
                 article: articleRemoved
             });
         });
-     
-    }
+
+    },
+
+    upload: (req, res) => {
+
+        //recoger el fichero
+        var file_name = 'Imagen no subida ...';
+
+        if (!req.files) {
+            return res.status(404).send({
+                status: 'error',
+                message: file_name
+            });
+        }
+        //conseguir el nombre y la extension del archivo
+        var file_path = req.files.file0.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+        var extension_split = file_name.split('\.');
+        var file_ext = extension_split[1];
+        //comprobar la extension, solo imagenes
+        if (file_ext != 'png' && file_ext != 'jpg' && file_ext != 'jpeg' && file_ext != 'gif') {
+            fs.unlink(file_path, (err) => {
+                return res.status(200).send({
+                    status: 'error',
+                    message: 'La extension de la imagen no es valida'
+                });
+            })
+        } else {
+            var articleId = req.params.id;
+
+            Article.findOneAndUpdate({ _id: articleId }, { image: file_name },
+                { new: true }, (err, articleUpdated) => {
+
+
+                    if(err || !articleUpdated){
+                        return res.status(404).send({
+                            status: 'error',
+                            message: 'error al guardar la imagen'
+                        });
+                    }
+                    return res.status(200).send({
+                        status: 'sucess',
+                        article: articleUpdated
+                    });
+                });
+
+        }
+        //si todo es valido buscar un articulo y asignar la imagen
+
+    },
+
+    getImage: (req,res)=>{
+
+        var file = req.params.image;
+        var path_file = './upload/articles/'+file;
+
+        fs.access(path_file,fs.constants.F_OK,(err)=>{
+            if(!err){
+                return res.sendFile(path.resolve(path_file));
+
+            }else{
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'La imagen no existe'
+                });
+
+            }
+        })
+    },
+
+    
 
 };
 
--- routes/article.js
+++ routes/article.js
@@ -4,7 +4,9 @@ var express = require('express');
 var ArticleController = require('../controllers/article');
 
 var router = express.Router();
-
+//configurar el modulo multiparty
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './upload/articles'});
 //rutas de prueba
 router.get('/datos_prueba', ArticleController.datosPrueba);
 router.get('/test',ArticleController.test);
@@ -15,6 +17,7 @@ router.get('/articles/:last?',ArticleController.getArticles);
 router.get('/article/:id',ArticleController.getArticle);
 router.put('/article/:id',ArticleController.update);
 router.delete('/article/:id',ArticleController.delete);
-
-
+router.post('/upload-image/:id', md_upload, ArticleController.upload);
+router.get('/get-image/:image', ArticleController.getImage);
+router.get('/search/:search',ArticleController.search);
 module.exports = router;
